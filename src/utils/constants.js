4918655e756a37ce072aa3112693b5b2d1b49b60
Anna Shatilova <anxet@mail.ru>
2024-03-09 20:23:17 +0300

    Корректный url для запроса к апи

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://api.valantis.store:40000/'
+export const BASE_URL = 'https://api.valantis.store:41000/'
 export const PASSWORD = 'Valantis'
 export const PER_PAGE = 50
